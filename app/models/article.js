a0561bae0d9dcff21ca62b5c94ba6812f418f802
Benjamin T. Seaver and Stella Huayhuaca <stella@>
2017-03-21 10:56:20 -0700

    Create blurb field as portion of story

--- app/models/article.js
+++ app/models/article.js
@@ -6,5 +6,9 @@ export default DS.Model.extend({
   picture: DS.attr(),
   byline: DS.attr(),
   story: DS.attr(),
-  release: DS.attr()
+  release: DS.attr(),
+
+  blurb: Ember.computed('story', function() {
+    return this.get('story').substr(0,200) + "...";
+  })
 });
