20f578b65ee62b110b02afd0af7136515fa9b1e1
Benjamin T. Seaver and Stella Huayhuaca <benjamin@>
2017-03-21 13:30:05 -0700

    Enable add new story

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,12 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('article');
   },
+
+  actions: {
+    saveArticle(params) {
+      var newArticle = this.store.createRecord('article', params);
+      newArticle.save();
+      this.transitionTo('index');
+    }
+  }
 });
